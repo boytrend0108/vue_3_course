fd23adfafe25dc8427105d8f66091a8a3b603877
boytrend <boytrend@gmail.com>
2022-11-10 15:56:49 +0400

    add module Dialog

---
+++ vue_3_course/src/components/UI/MyDialog.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="dialog" v-if="show" @click="hideDialog">
+        <!--on click don't work with this div-->
+        <div @click.stop class="dialog__content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'my-dialog',// need to go to index.js and add this component
+    props: {
+        show: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        hideDialog() {
+            this.$emit('update:show', false)
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.dialog {
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    display: flex;
+}
+
+.dialog__content {
+    margin: auto;
+    background: white;
+    border-radius: 12px;
+    min-width: 350px;
+    min-height: 50px;
+    padding: 20px;
+}
+</style>
--- vue_3_course/src/components/UI/index.js
+++ vue_3_course/src/components/UI/index.js
@@ -2,8 +2,11 @@
 
 import MyButton from '@/components/UI/MyButton'
 import Myinput from "@/components/UI/MyInput"
+import MyDialog from "@/components/UI/MyDialog"
+
 
 export default [// here ARRAY
     MyButton,
-    Myinput
+    Myinput,
+    MyDialog
 ]
