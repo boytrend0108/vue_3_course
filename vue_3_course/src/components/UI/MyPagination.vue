1347d338ba2f14020f3cfab71e6f1e1be79d4f88
boytrend <boytrend@gmail.com>
2022-11-12 11:29:36 +0400

    add animation for button

--- vue_3_course/src/components/UI/MyPagination.vue
+++ vue_3_course/src/components/UI/MyPagination.vue
@@ -1,17 +1,17 @@
 <template>
 <!--this is iterate over number "v-for="pageNumber in totalPage""-->
 <!--We also can bild class ":class="{ current__page: true }"-->
-<div class="page__wrapper">     
+<div class="page__wrapper">
     <div class="page" 
     v-for="pageNumber in totalPage" 
     :key="pageNumber" 
-    :class="{ current__page: page === pageNumber }" 
+    :class="{ current__page: page === pageNumber   }" 
     @click="$emit('changePage', pageNumber)">
     <!--page === pageNumber- is Boolean-->
     {{ pageNumber }}
     </div>
-    <div></div>
 </div>
+
 </template>
 
 <script>
@@ -46,10 +46,41 @@ export default {
     width: 30px;
     text-align: center;
 }
+.page:hover{
+    scale: 1.2;
+}
 
 .current__page {
     border: 2px solid teal;
     color: white;
     background: teal;
+}
+.slide-top {
+    -webkit-animation: slide-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+    animation: slide-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+}
+
+@-webkit-keyframes slide-top {
+    0% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+
+    100% {
+        -webkit-transform: translateY(-100px);
+        transform: translateY(-100px);
+    }
+}
+
+@keyframes slide-top {
+    0% {
+        -webkit-transform: translateY(0);
+        transform: translateY(0);
+    }
+
+    100% {
+        -webkit-transform: translateY(-100px);
+        transform: translateY(-100px);
+    }
 }
 </style>
